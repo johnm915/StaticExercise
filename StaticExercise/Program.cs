d694e9e4a6e6d7c1e3561332185af8cee4ed8ab2
john morgangit config --global user.email jmm915@gmail.com <jmm915@gmail.com>
2022-02-12 13:41:44 -0600

    Static

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -7,11 +7,13 @@ namespace StaticExercise
         static void Main(string[] args)
         {
 
-           var celcius = TempConverter.FahernheitToCelsius(68);
-            var fahernheit = TempConverter.CelsiusToFaherhenit(20);
+         var celcius = TempConverter.FahernheitToCelsius(68);
+         var fahernheit = TempConverter.CelsiusToFaherhenit(20);
 
             Console.WriteLine($"celcius {celcius}");
             Console.WriteLine($"fehernheit {fahernheit}");
+            Console.WriteLine();
+
 
         }
     }
